69c84c647eed47d40b3f6dda40b1d4276219e223
WizardPenguin77 <andrewchung669@gmail.com>
2025-08-29 13:06:20 -0700

    Added java code program

---
+++ GITReady.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class GITReady {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int numberToGuess = random.nextInt(100) + 1; // 1 to 100
+        int guess = 0;
+        int attempts = 0;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I'm thinking of a number between 1 and 100.");
+
+        while (guess != numberToGuess) {
+            System.out.print("Enter your guess: ");
+            if (scanner.hasNextInt()) {
+                guess = scanner.nextInt();
+                attempts++;
+
+                if (guess < numberToGuess) {
+                    System.out.println("Too low! Try again.");
+                } else if (guess > numberToGuess) {
+                    System.out.println("Too high! Try again.");
+                } else {
+                    System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+                }
+            } else {
+                System.out.println("Please enter a valid number.");
+                scanner.next(); // discard invalid input
+            }
+        }
+
+        scanner.close();
+    }
+}
